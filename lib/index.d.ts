732a6dfa0b5e56547dd76e934567772bc3547030
Mahmoud Ali <ma7moud.saraya@gmail.com>
2025-02-08 22:41:06 +0200

    feat: add Zendesk integration with initialization and article viewing functions

---
+++ lib/index.d.ts
@@ -0,0 +1,23 @@
+interface Config {
+    appId: string;
+    clientId: string;
+    zendeskUrl: string;
+}
+export declare function initialize(config: Config): void;
+export declare function identifyJWT(token: string): void;
+export declare function identifyAnonymous(name?: string, email?: string): void;
+interface HelpCenterOptions {
+    hideContactSupport?: boolean;
+}
+interface ViewArticleOptions {
+    hideContactSupport?: boolean;
+    articleId?: string;
+}
+export declare function showHelpCenter(options: HelpCenterOptions): void;
+interface NewTicketOptions {
+    tags?: string[];
+}
+export declare function showNewTicket(options: NewTicketOptions): void;
+export declare function showTicketList(): void;
+export declare function showArticle(options: ViewArticleOptions): void;
+export {};
